4a8da6b015c3c66039826ca0665d342436868d1f
raj <raj229500@gmail.com>
2025-11-20 17:46:48 +0530

    update

--- server.js
+++ server.js
@@ -11,21 +11,52 @@ const app = express();
 app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(express.json());
 
-// Simple insert API
+// Monthly feedback insert API
 app.post("/submit", async (req, res) => {
-  const { first_name, last_name, email, phone, student_id, description } = req.body;
+  const {
+    studentName,
+    joiningCourse,
+    batchTime,
+    teacherName,
+    q1,
+    q2,
+    q3,
+    q4,
+    q5,
+    q6,
+    suggestion
+  } = req.body;
 
   try {
     const query = `
-      INSERT INTO feedback(first_name, last_name, email, phone, student_id, description)
-      VALUES($1, $2, $3, $4, $5, $6) RETURNING *;
+      INSERT INTO feedback(
+        student_name,
+        joining_course,
+        batch_time,
+        teacher_name,
+        q1, q2, q3, q4, q5, q6,
+        suggestion
+      )
+      VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)
+      RETURNING *;
     `;
 
-    const values = [first_name, last_name, email, phone, student_id, description];
+    const values = [
+      studentName,
+      joiningCourse,
+      batchTime,
+      teacherName,
+      q1, q2, q3, q4, q5, q6,
+      suggestion
+    ];
 
     const result = await pool.query(query, values);
 
-    res.json({ message: "Feedback submitted successfully!", data: result.rows[0] });
+    res.json({
+      message: "Monthly Feedback saved successfully!",
+      data: result.rows[0]
+    });
+
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Error occurred!", error: err.message });
